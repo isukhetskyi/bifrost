e5eda6e9720359d72a092f417c4af92599768cdf
isukhetskyi <ivan.sukhetskyi@gmail.com>
2017-10-30 23:58:33 +0200

    Implementaion of readonly repository

--- src/Bifrost.Repository/EntityFrameworkReadOnlyRepository.cs
+++ src/Bifrost.Repository/EntityFrameworkReadOnlyRepository.cs
@@ -16,75 +16,113 @@ namespace Bifrost.Repository
         {
             this.context = context;
         }
+        protected virtual IQueryable<TEntity> GetQueryable<TEntity>(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = null,
+            int? skip = null,
+            int? take = null) where TEntity : class, IEntity
+            {
+                includeProperties = includeProperties ?? string.Empty;
+                IQueryable<TEntity> query = context.Set<TEntity>();
 
-        public IEnumerable<TEntity> Get<TEntity>(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null, int? skip = null, int? take = null) where TEntity : class, IEntity
+                if (filter != null)
+                {
+                    query = query.Where(filter);
+                }
+
+                foreach(var includeProperty in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+
+                if (orderBy != null)
+                {
+                    query = orderBy(query);
+                }
+
+                if(skip.HasValue)
+                {
+                    query = query.Skip(skip.Value);
+                }
+
+                if(take.HasValue)
+                {
+                    query = query.Take(take.Value);
+                }
+
+                return query;
+            }
+
+
+        public virtual  IEnumerable<TEntity> Get<TEntity>(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null, int? skip = null, int? take = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return GetQueryable(filter, orderBy, includeProperties, skip, take).ToList();
         }
 
-        public IEnumerable<TEntity> GetAll<TEntity>(Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null, int? skip = null, int? take = null) where TEntity : class, IEntity
+        public virtual  IEnumerable<TEntity> GetAll<TEntity>(Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null, int? skip = null, int? take = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return GetQueryable(null, orderBy, includeProperties, skip, take).ToList();
         }
 
-        public Task<IEnumerable<TEntity>> GetAllAsync<TEntity>(Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null, int? skip = null, int? take = null) where TEntity : class, IEntity
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync<TEntity>(Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null, int? skip = null, int? take = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return await GetQueryable(null, orderBy, includeProperties, skip, take).ToListAsync();
         }
 
-        public Task<IEnumerable<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null, int? skip = null, int? take = null) where TEntity : class, IEntity
+        public virtual async Task<IEnumerable<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null, int? skip = null, int? take = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return await GetQueryable(filter, orderBy, includeProperties, skip, take).ToListAsync();
         }
 
-        public TEntity GetById<TEntity>(object id) where TEntity : class, IEntity
+        public virtual  TEntity GetById<TEntity>(object id) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return context.Set<TEntity>().Find(id);
         }
 
-        public Task<TEntity> GetByIdAsync<TEntity>(object id) where TEntity : class, IEntity
+        public virtual async Task<TEntity> GetByIdAsync<TEntity>(object id) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return await context.Set<TEntity>().FindAsync(id);
         }
 
-        public int GetCount<TEntity>(Expression<Func<TEntity, bool>> filter = null) where TEntity : class, IEntity
+        public virtual int GetCount<TEntity>(Expression<Func<TEntity, bool>> filter = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return GetQueryable(filter).Count();
         }
 
-        public Task<int> GetCountAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null) where TEntity : class, IEntity
+        public virtual async Task<int> GetCountAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return await GetQueryable(filter).CountAsync();
         }
 
-        public bool GetExists<TEntity>(Expression<Func<TEntity, bool>> filter = null) where TEntity : class, IEntity
+        public virtual  bool GetExists<TEntity>(Expression<Func<TEntity, bool>> filter = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return GetQueryable(filter).Any();
         }
 
-        public Task<bool> GetExistsAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null) where TEntity : class, IEntity
+        public virtual async Task<bool> GetExistsAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return await GetQueryable(filter).AnyAsync();
         }
 
-        public TEntity GetFirst<TEntity>(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null) where TEntity : class, IEntity
+        public virtual TEntity GetFirst<TEntity>(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return GetQueryable(filter, orderBy,includeProperties).FirstOrDefault();
         }
 
-        public Task<TEntity> GetFirstAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null) where TEntity : class, IEntity
+        public virtual async Task<TEntity> GetFirstAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return await GetQueryable(filter, orderBy, includeProperties).FirstOrDefaultAsync();
         }
 
-        public TEntity GetOne<TEntity>(Expression<Func<TEntity, bool>> filter = null, string includeProperties = null) where TEntity : class, IEntity
+        public virtual TEntity GetOne<TEntity>(Expression<Func<TEntity, bool>> filter = null, string includeProperties = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return GetQueryable<TEntity>(filter, null, includeProperties).SingleOrDefault();
         }
 
-        public Task<TEntity> GetOneAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null, string includeProperties = null) where TEntity : class, IEntity
+        public virtual async Task<TEntity> GetOneAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null, string includeProperties = null) where TEntity : class, IEntity
         {
-            throw new NotImplementedException();
+            return await GetQueryable<TEntity>(filter, null, includeProperties).SingleOrDefaultAsync();
         }
     }
 }
