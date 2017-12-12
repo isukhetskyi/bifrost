1ac78631ad5ba0e04b899e4d68ea1dc6f382233b
isukhetskyi <ivan.sukhetskyi@gmail.com>
2017-12-12 22:59:41 +0200

    Added web app for bifrost app

--- src/Bifrost.WebApp/Program.cs
+++ src/Bifrost.WebApp/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Bifrost.Web
+namespace Bifrost.WebApp
 {
     public class Program
     {
