ba045e0ccbc78fa3fffbea3a494b8847864ba449
Ivan Sukhetskyi <ivan.sukhetskyi@gmail.com>
2018-02-18 22:27:21 +0200

    Migration

--- src/Bifrost.Data/Migrations/20180218202501_AuthData.Designer.cs
+++ src/Bifrost.Data/Migrations/20180218202501_AuthData.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Bifrost.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180218202501_AuthData")]
+    partial class AuthData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
