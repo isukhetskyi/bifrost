4092d2b7b197b839631a01e2d075dbcd394867c8
isukhetskyi <ivan.sukhetskyi@gmail.com>
2018-02-22 01:29:34 +0200

    Added library why-did-you-update

--- src/Bifrost.Web/ClientApp/boot.tsx
+++ src/Bifrost.Web/ClientApp/boot.tsx
@@ -5,17 +5,20 @@ import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { BrowserRouter } from 'react-router-dom';
 import * as RoutesModule from './routes';
+import {whyDidYouUpdate} from "why-did-you-update";
 let routes = RoutesModule.routes;
 
 function renderApp() {
     // This code starts up the React app when it runs in a browser. It sets up the routing
     // configuration and injects the app into a DOM element.
     const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')!;
+    whyDidYouUpdate(React);
     ReactDOM.render(
         <AppContainer>
             <BrowserRouter children={ routes } basename={ baseUrl } />
         </AppContainer>,
         document.getElementById('react-app')
+
     );
 }
 
