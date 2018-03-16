fec13435f93245fcafa7b56d83441aecf9c554ad
isukhetskyi <ivan.sukhetskyi@gmail.com>
2018-03-16 14:35:30 +0200

    Added dropdowns for the filter

--- src/Bifrost.Web/ClientApp/components/Respondents.tsx
+++ src/Bifrost.Web/ClientApp/components/Respondents.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps } from 'react-router';
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import * as axios from 'axios';
+import { CustomSelect } from "./shared/CustomSelect";
 
 class Respondent {
     Id?: number;
@@ -18,7 +19,10 @@ class Respondent {
 }
 
 interface RespondentsState {
-    data: Array<Respondent>
+    data: Array<Respondent>;
+    ProgrammingLanguages: Array<[string, string]>;
+    Frameworks: Array<[string, string]>;
+    Databases: Array<[string, string]>;
 }
 
 export class Respondents extends React.Component<RouteComponentProps<{}>, RespondentsState> {
@@ -26,15 +30,21 @@ export class Respondents extends React.Component<RouteComponentProps<{}>, Respon
         super();
 
         this.state = {
-            data: []
+            data: [],
+            ProgrammingLanguages:[],
+            Frameworks:[],
+            Databases:[]
         }
+
+        this.handleDropdownChange = this.handleDropdownChange.bind(this);
+        this.initializeData = this.initializeData.bind(this);
     }
 
     componentDidMount(){
         let respondents: any;
         let thisContext = this;
 
-        axios.default.get("/Respondents/All")
+        axios.default.get("/respondents/all")
             .then(function(response){
                 console.log(response);
                 respondents = response.data.respondents;
@@ -43,13 +53,89 @@ export class Respondents extends React.Component<RouteComponentProps<{}>, Respon
                 console.error(error);
             });
 
-
+        axios.default.get("/survey/gettechnologies")
+            .then(function(response){
+                    thisContext.setState(
+                        {
+                            ProgrammingLanguages: response.data.technologies.languages.map(
+                                (item: any) =>
+                                    [item.id, item.technologyName])
+                        })
+                    thisContext.setState(
+                        {
+                            Databases: response.data.technologies.databases.map(
+                                (item: any) =>
+                                    [item.id, item.technologyName])
+                        })
+                    thisContext.setState(
+                        {
+                            Frameworks: response.data.technologies.frameworks.map(
+                                (item: any) =>
+                                    [item.id, item.technologyName])
+                        })
+                        thisContext.initializeData();
+            })
+            .catch(function(error){
+                console.log(error);
+                alert(error);
+            })
     }
+
+    handleDropdownChange(e: any){
+        if(e.target.value as number >= 0){
+            console.log(e.target.options[e.target.selectedIndex].text);
+        }
+    }
+
+    initializeData(){
+        let context = this;
+        let currentArrayValue = this.state.ProgrammingLanguages;
+        currentArrayValue.unshift(["0", "Choose"])
+        this.setState({ProgrammingLanguages: currentArrayValue})
+
+        currentArrayValue = this.state.Databases;
+        currentArrayValue.unshift(["0", "Choose"])
+        this.setState({Databases: currentArrayValue});
+
+        currentArrayValue = this.state.Frameworks;
+        currentArrayValue.unshift(["0", "Choose"])
+        this.setState({Frameworks: currentArrayValue});
+    }
+
 
     public render() {
         const data = this.state.data;
+
         return <div style={{width:"100%"}}>
             <h2 className="text-center">Respondents</h2>
+                <div className="row" style={{marginBottom: 30}}>
+                    <CustomSelect classes="col-md-4"
+                    id="LanguagesSelect"
+                    handleDropdownChange={this.handleDropdownChange}
+                    options={this.state.ProgrammingLanguages}
+                    selectId="LanguageSelect"
+                    selectTitle="Programming language filter"
+                    >
+                </CustomSelect>
+                <CustomSelect classes="col-md-4"
+                    id="FrameworkSelect"
+                    handleDropdownChange={this.handleDropdownChange}
+                    options={this.state.Frameworks}
+                    selectId="FrameworkSelect"
+                    selectTitle="Framework filter"
+                    >
+                </CustomSelect>
+                <CustomSelect classes="col-md-4"
+                    id="DatabasesSelect"
+                    handleDropdownChange={this.handleDropdownChange}
+                    options={this.state.Databases}
+                    selectId="DatabaseSelect"
+                    selectTitle="Database filter"
+                    >
+                </CustomSelect>
+            </div>
+
+
             <ReactTable
                 data={data as any}
                 filterable
@@ -82,6 +168,8 @@ export class Respondents extends React.Component<RouteComponentProps<{}>, Respon
                         Header: "Technologies",
                         accessor: "technologies",
                         style: { "textAlign": "center" },
+                        /// <reference path="TechnologiesFilter" />
+
                         filterMethod: (filter: any, row: any) =>
                             (row[filter.id].toLowerCase() as string).indexOf(filter.value.toLowerCase() as string) >= 0
                     },
